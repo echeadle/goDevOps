ea525e98c220c4591f433e9331aafbb08388fe1c
Edward Cheadle <edward.cheadle@cambiahealth.com>
2018-04-17 16:16:36 -0600

    Added post request to file

--- requests/requests.go
+++ requests/requests.go
@@ -5,10 +5,14 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	resp, err := http.Get("https://httpbin.org/get?search=something")
+	// resp, err := http.Get("https://httpbin.org/get")
+	// resp, err := http.Get("https://httpbin.org/get?search=something")
+	resp, err := http.Post("https://httpbin.org/post", "text/plain",
+		strings.NewReader("this is thhe request content"))
 	if err != nil {
 		log.Fatalln("Unable to get response")
 	}
