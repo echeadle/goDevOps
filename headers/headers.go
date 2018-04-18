1776dcbdaeb07b1946e4a1acd30fe1621e4c784f
Edward Cheadle <edward.cheadle@cambiahealth.com>
2018-04-17 18:13:33 -0600

    First headers.go using get does not work

---
+++ headers/headers.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://httpbin.org/basic-auth/user/passw0rd")
+	if err != nil {
+		log.Fatalln("Unable to get response")
+	}
+	defer resp.Body.Close()
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln("Unable to read content")
+	}
+	fmt.Println(string(content))
+	fmt.Println(resp.StatusCode)
+}
